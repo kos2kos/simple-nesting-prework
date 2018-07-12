21f881ffb89182af9b1ff8d051138b6a818ab35d
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-12 02:25:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -85,7 +85,7 @@ def adding_matz
      programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
     :languages => ["LISP, C"]}
 
-    
+    programmer_hash[:matz] =
 end
 
 def changing_alan
