f634e20f92ff6a0bdbf12df2a705867b5f57cfff
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-12 02:37:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,12 +85,7 @@ def adding_matz
      programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
     :languages => ["LISP, C"]}
 
-    programmer_hash[:matz] = {
-          :known_for => "",
-          :languages => []
-      :yukihiro_matsumoto =>{:known_for => "Ruby",
-    :languages => ["LISP, C"]
-    }}
+    
 end
 
 def changing_alan
