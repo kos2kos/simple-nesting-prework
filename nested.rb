0fb6e003e0b85f76f310dfe7fa2253c0cccb0de1
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-12 02:47:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,7 @@ def adding_matz
 end
 
 def changing_alan
-	programmer_hash = 
+		programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -107,7 +107,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
     programmer_hash[:alan_kay][:known_for] = alans_new_info
-    alans_new_info
 end
 
 def adding_to_dennis
